56e1e79fd8903ffb4576a9c70fd130b2b1a7df24
Max <thesugarhoneyicetea@hotmail.com>
2020-11-10 12:33:52 +0800

    truck validations

--- spec/truck_spec.rb
+++ spec/truck_spec.rb
@@ -4,23 +4,24 @@ describe Truck do
   describe 'validations' do
     subject { truck.valid? }
 
-    let(:truck) { Truck.new(license_plate: license_plate, capacity: capacity) }
+    let(:truck) { Truck.new(license_plate: license_plate, capacity: capacity, status: status) }
     let(:license_plate) { '1234' }
     let(:capacity) { 100 }
+    let(:status) { :active }
 
     context 'license plate is not present' do
       let(:license_plate) { '' }
 
-      it { is_expected.to be_false }
+      it { is_expected.to be_falsey }
     end
     
     context 'capacity is not present' do
       let(:capacity) { nil }
 
-      it { is_expected.to be_false }
+      it { is_expected.to be_falsey }
     end
 
-    it { is_expected.to be_true }
+    it { is_expected.to be_truthy }
   end 
 
   # describe 'validations with shoulda-matchers' do

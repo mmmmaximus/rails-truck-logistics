0682f17e67c07c9065d2e148ce22df10ab8d5d87
Max <thesugarhoneyicetea@hotmail.com>
2020-11-12 16:17:10 +0800

    change random decimal generator

--- spec/factories/cargos.rb
+++ spec/factories/cargos.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     description { FFaker::Lorem.paragraph }
     title { FFaker::Lorem.word }
     reference_number { rand(0..999999) }
-    value { FFaker.numerify("#.##") }
+    value { rand(0.00..10_000.00).round(2) }
   end
 end

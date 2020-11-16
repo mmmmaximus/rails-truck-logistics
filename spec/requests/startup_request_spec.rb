ed8f9936adcd8ea57b8093e34eec4f8b5fb20fef
Max <thesugarhoneyicetea@hotmail.com>
2020-11-16 18:55:04 +0800

    changed table

--- spec/requests/startup_request_spec.rb
+++ spec/requests/startup_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Startups", type: :request do
-
+describe "Startups" do
   describe "GET /index" do
     it "returns http success" do
       get "/startup/index"
       expect(response).to have_http_status(:success)
     end
   end
-
 end

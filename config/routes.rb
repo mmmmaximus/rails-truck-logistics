55dc4462621044ac86a468532aa14c19f0020b4b
Max <thesugarhoneyicetea@hotmail.com>
2020-11-17 15:35:05 +0800

    added driver index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :startup, only: [:index]
   root to: 'startup#index'
-  resources :trucks
+  resources :trucks, :drivers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

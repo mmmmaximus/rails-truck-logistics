9a398b38efef8e96b05a531a137f258b9d2ba8ba
Max <thesugarhoneyicetea@hotmail.com>
2020-11-18 14:47:11 +0800

    add cargo index, fix driver routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :trucks
   resources :drivers
   resources :routes
+  resources :cargos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

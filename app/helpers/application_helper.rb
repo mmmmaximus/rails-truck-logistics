a587d2afce33259349fda70daa07bfda668df9c4
Max <thesugarhoneyicetea@hotmail.com>
2020-11-18 18:19:53 +0800

    change helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def model_attributes(klass)
-    @model_attributes ||= klass.column_names.reject{|col| col.include?("_id") || col.include?("created_at") || col.include?("updated_at")}
+    @model_attributes ||= klass.column_names.reject do |col|
+      col.include?("_id") || col.include?("created_at") || col.include?("updated_at")
+    end
   end
 end

4fa2cf76297f548b8baf2032acfb04b9ed78d1c2
Max <thesugarhoneyicetea@hotmail.com>
2020-11-19 14:37:03 +0800

    remove comment

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,7 +5,6 @@ describe ApplicationHelper do
     subject { helper.model_attributes(klass) }
     let(:klass) { Truck }
     let(:expected_array) do
-      # put something in here to verify it
       ["id", "license_plate", "capacity", "color", "service_date", "status"]
     end
 

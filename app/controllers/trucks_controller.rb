abb64fd196c62583ac059ce724125fad74f23ad9
Max Woon <thesugarhoneyicetea@hotmail.com>
2020-11-24 11:21:15 +0800

    added truck CRUD (#16)
    * added truck CRUD
    
    * add include_blank

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -5,7 +5,45 @@ class TrucksController < ApplicationController
     @model_attributes = model_attributes(Truck)
   end
 
+  def new
+    @truck = Truck.new
+  end
+
+  def create
+    truck = Truck.new(truck_params)
+
+    if truck.save
+      redirect_to trucks_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @truck = Truck.find(params[:id])
+  end
+
+  def update
+    truck = Truck.find(params[:id])
+
+    if truck.update(truck_params)
+      redirect_to trucks_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    truck = Truck.find(params[:id])
+    truck.destroy
+
+    redirect_to trucks_path
+  end
+
   private
+  def truck_params
+    params.require(:truck).permit(:license_plate, :capacity, :color, :service_date, :status, :model_type_id)
+  end
 
   def trucks
     @trucks ||= Truck.all

d5c500cb97a48bccf6b9747d7473f85c8a8c39d2
Max Woon <thesugarhoneyicetea@hotmail.com>
2020-11-24 20:51:49 +0800

    add delete driver (#18)

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,8 +5,14 @@ class DriversController < ApplicationController
     @model_attributes = model_attributes(Driver)
   end
 
-  private
+  def destroy
+    driver = Driver.find(params[:id])
+    driver.destroy
 
+    redirect_to drivers_path
+  end
+
+  private
   def drivers
     @drivers ||= Driver.all
   end

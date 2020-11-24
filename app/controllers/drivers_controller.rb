25b29161aeefd6d225ccdd585e2c2ccd7206f45a
Max <thesugarhoneyicetea@hotmail.com>
2020-11-24 14:57:26 +0800

    add driver new, create

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,7 +5,25 @@ class DriversController < ApplicationController
     @model_attributes = model_attributes(Driver)
   end
 
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    driver = Driver.new(driver_params)
+    debugger
+
+    if driver.save
+      redirect_to drivers_path
+    else
+      render 'new'
+    end
+  end
+
   private
+  def driver_params
+    params.require(:driver).permit(:name, :license_number, :mobile_number, :email_address, :age, :notes, :status)
+  end
 
   def drivers
     @drivers ||= Driver.all

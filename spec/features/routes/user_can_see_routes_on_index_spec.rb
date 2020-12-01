38eb0846b631b94c0d2b11f37a8edd82ad2e75c9
Max <thesugarhoneyicetea@hotmail.com>
2020-12-01 14:53:48 +0800

    renamed scenarios

--- spec/features/routes/user_can_see_routes_on_index_spec.rb
+++ spec/features/routes/user_can_see_routes_on_index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'user can see routes in index spec' do
   let!(:route) { create(:route) }
 
-  scenario 'user can see route spec' do
+  scenario 'user can see route' do
     visit(routes_path)
     expect(page).to have_content(route.name)
     expect(page).to have_content(route.length_of_time)

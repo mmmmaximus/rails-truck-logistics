51a92911246f1daaf0c2c79224d45eace6b434e8
Max <thesugarhoneyicetea@hotmail.com>
2020-12-01 22:09:31 +0800

    add model type CRUD and feature tests

---
+++ spec/features/model_types/user_can_edit_model_type_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'user can edit model type' do
+  let!(:model_type) { create(:model_type) }
+
+  background do
+    visit(model_types_path)
+    click_link('Edit')
+  end
+
+  scenario 'user can edit model type specs' do
+    fill_in('Name', with: 'name')
+    fill_in('Brand', with: 'brand')
+    click_button('Update Model type')
+
+    expect(page).to have_content('Model Types Index')
+    expect(current_path).to eq(model_types_path)
+
+    within('tbody') do
+      within(all('tr')[0]) do
+        expect(page).to have_content('name')
+        expect(page).to have_content('brand')
+      end
+    end
+
+    model_type.reload
+    expect(model_type.name).to eq('name')
+    expect(model_type.brand).to eq('brand')
+  end
+end

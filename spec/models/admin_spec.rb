d16a27be29c0468765b63f2801a39353464263a5
Max <thesugarhoneyicetea@hotmail.com>
2020-12-16 15:39:05 +0800

    add password presence validation

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 describe Admin do
-  describe 'admin validations' do
-    subject { build(:admin) }
-    it { is_expected.to be_valid }
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+  describe 'admin' do
+    context 'validations' do
+      subject { build(:admin) }
+      it { is_expected.to be_valid }
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    end
+
+    context 'password presence validation' do
+      subject { build(:admin, password: nil) }
+      it { is_expected.to validate_presence_of(:password) }
+    end
   end
 end

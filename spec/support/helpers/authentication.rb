190e68818c1cf6e9924aa92474c4d7019c5ad6af
Max <thesugarhoneyicetea@hotmail.com>
2020-12-18 17:34:04 +0800

    clean up

--- spec/support/helpers/authentication.rb
+++ spec/support/helpers/authentication.rb
@@ -2,7 +2,7 @@ module Helpers
   module Authentication
     def log_in_as(user)
       visit(new_session_path)
-      fill_in("Email", with: "email@email.com")
+      fill_in("Email", with: user.email)
       fill_in("Password", with: "password")
       click_button("Login")
     end

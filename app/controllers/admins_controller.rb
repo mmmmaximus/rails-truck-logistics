cee1ddcc5dcbb0cbdffbdb55fcead581b111c658
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-08-09 14:19:16 +0800

    Revert "admin can delete own account"
    This reverts commit 3b6bd4219d62ebac7a715e397a7b0fafc019e076.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -46,15 +46,8 @@ class AdminsController < ApplicationController
   # DELETE /admins/1
   def destroy
     admin.destroy
-    if admin == current_admin
-      session[:admin_id] = nil
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: "admin: #{@admin.email} was successfully deleted." }
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to admins_path, notice: "admin: #{@admin.email} was successfully deleted." }
-      end
+    respond_to do |format|
+      format.html { redirect_to admins_path, notice: "admin: #{@admin.email} was successfully deleted." }
     end
   end
 

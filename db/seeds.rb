cf145d074eff930b0d6f428fbea0a591f02f9e4a
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-08-23 16:12:33 +0800

    seed data (#59)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Truck.destroy_all
+Driver.destroy_all
+Route.destroy_all
+Cargo.destroy_all
+ModelType.destroy_all
+
+FactoryBot.create_list(:truck, 3)
+FactoryBot.create_list(:driver, 3)
+FactoryBot.create_list(:route, 3)
+FactoryBot.create_list(:cargo, 3)
+FactoryBot.create_list(:model_type, 3)
